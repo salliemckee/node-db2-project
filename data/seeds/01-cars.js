c96c2e608826df1421cd33a7d94fbf12c098d5cc
Sallie McKee <salliemckee@Morrigan.local>
2022-04-19 15:57:22 -0500

    set up server and cars router

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "manual",
+  },
+
+  {
+    vin: "2222222222222",
+    make: "toyota",
+    model: "corolla",
+    mileage: 115000,
+    title: "salvage",
+  },
+
+  { vin: "33333333333333", make: "ford", model: "focus", mileage: 15000 },
+];
+
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(() => {
+      return knex("cars").insert(cars);
+    });
+};
